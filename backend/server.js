ef4d941bcd57d21f188444f328768d49b1651e00
Naveen P <pnaveen10@gmail.com>
2017-03-16 14:58:51 +0530

    React and Back-end merge

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
+var path = require('path');
+
 app.listen(3001, function() {
   console.log('listening on 3001')
 })
 
-app.get('/test', (req, res) => {
-	res.send("hai");
+app.use('/static', express.static(path.resolve(__dirname + '\\..\\react-ui\\build\\static')));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.resolve(__dirname + '\\..\\react-ui\\build\\index.html'));
 })

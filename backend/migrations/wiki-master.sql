cc4eec7ef67641fb259851f132bf0afadfee4d26
Dinesh Palanisamy <dpalanisamy@corp.xome.com>
2017-03-17 09:31:33 +0530

    [added created_at updated_at ]

--- backend/migrations/wiki-master.sql
+++ backend/migrations/wiki-master.sql
@@ -14,3 +14,10 @@ WITH (
 
 ALTER TABLE public.wiki_master
     OWNER to postgres;
+
+
+run this after the abouve query:
+
+ALTER TABLE public.wiki_master ADD COLUMN created_at time with time zone;
+ALTER TABLE public.wiki_master ALTER COLUMN created_at SET DEFAULT now();
+ALTER TABLE public.wiki_master ADD COLUMN updated_at time with time zone;

5afc9c83bdb0425a427fa9004461bbbba180ed2a
Naveen P <pnaveen10@gmail.com>
2017-03-17 11:26:37 +0530

    HAC-8 Temp components to start work on

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import $ from "jquery";
+import EditMode from './EditMode';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Wiki</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <EditMode/>
       </div>
     );
   }
